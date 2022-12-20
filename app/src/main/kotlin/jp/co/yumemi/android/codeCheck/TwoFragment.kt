bd87266e16e0365f15dd92064b0dbf89358177ff
Mw-majo <ritz1229@icloud.com>
2022-12-20 11:31:22 +0900

    fix: project name
    code_checkとなっていたが、プロジェクト名はキャメルケースで書くため、関連する部分を修正した

--- app/src/main/kotlin/jp/co/yumemi/android/codeCheck/TwoFragment.kt
+++ app/src/main/kotlin/jp/co/yumemi/android/codeCheck/TwoFragment.kt
@@ -1,16 +1,15 @@
 /*
  * Copyright © 2021 YUMEMI Inc. All rights reserved.
  */
-package jp.co.yumemi.android.code_check
+package jp.co.yumemi.android.codeCheck
 
 import android.os.Bundle
 import android.util.Log
 import android.view.View
 import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.navArgs
-import coil.load
-import jp.co.yumemi.android.code_check.TopActivity.Companion.lastSearchDate
-import jp.co.yumemi.android.code_check.databinding.FragmentTwoBinding
+import jp.co.yumemi.android.codeCheck.TopActivity.Companion.lastSearchDate
+import jp.co.yumemi.android.codeCheck.databinding.FragmentTwoBinding
 
 class TwoFragment : Fragment(R.layout.fragment_two) {
 
