b89f18309b86724f919d469d3fc28467ffef9eb5
Mw-majo <ritz1229@icloud.com>
2022-12-22 15:51:42 +0900

    rebase 'main' to 'origin/main'

--- app/src/main/kotlin/jp/co/yumemi/android/codeCheck/SearchViewModel.kt
+++ app/src/main/kotlin/jp/co/yumemi/android/codeCheck/SearchViewModel.kt
@@ -27,6 +27,7 @@ class SearchViewModel(val context: Context) : ViewModel() {
         val client = HttpClient(Android)
 
         return@runBlocking GlobalScope.async {
+
             val response: HttpResponse =
                 client?.get("https://api.github.com/search/repositories") {
                     header("Accept", "application/vnd.github.v3+json")
