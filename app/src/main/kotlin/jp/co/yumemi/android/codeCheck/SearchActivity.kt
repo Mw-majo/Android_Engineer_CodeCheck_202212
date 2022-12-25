0cdcf987ef594407049389af94aab4def22b2d4b
Mw-majo <ritz1229@icloud.com>
2022-12-25 11:51:39 +0900

    fix: lastSearchDateをSearchActivityからSearchViewModelに移動
    データはアクティビティに保持すべきではないから
    
    課題：
    livedataに保存したDateが正しく保存されていない
    logに出てくる値がnullになっている

--- app/src/main/kotlin/jp/co/yumemi/android/codeCheck/SearchActivity.kt
+++ app/src/main/kotlin/jp/co/yumemi/android/codeCheck/SearchActivity.kt
@@ -6,9 +6,4 @@ package jp.co.yumemi.android.codeCheck
 import androidx.appcompat.app.AppCompatActivity
 import java.util.*
 
-class SearchActivity : AppCompatActivity(R.layout.activity_search) {
-
-    companion object {
-        lateinit var lastSearchDate: Date
-    }
-}
+class SearchActivity : AppCompatActivity(R.layout.activity_search)
--- app/src/main/kotlin/jp/co/yumemi/android/codeCheck/SearchResultFragment.kt
+++ app/src/main/kotlin/jp/co/yumemi/android/codeCheck/SearchResultFragment.kt
@@ -7,10 +7,11 @@ import android.os.Bundle
 import android.util.Log
 import android.view.View
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
 import androidx.navigation.fragment.navArgs
 import coil.load
-import jp.co.yumemi.android.codeCheck.SearchActivity.Companion.lastSearchDate
 import jp.co.yumemi.android.codeCheck.databinding.FragmentSearchResultBinding
+import java.util.*
 
 class SearchResultFragment : Fragment(R.layout.fragment_search_result) {
 
@@ -19,19 +20,23 @@ class SearchResultFragment : Fragment(R.layout.fragment_search_result) {
     private var _binding: FragmentSearchResultBinding? = null
     private val binding get() = _binding!!
 
+    //private val viewModel: SearchViewModel by viewModels()
+
+
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        Log.d("検索した日時", lastSearchDate.toString())
+        val searchViewModel = SearchViewModel(requireContext())
+        Log.d("検索した日時", searchViewModel.lastSearchDate.value.toString())
 
         val item = args.item
         _binding = FragmentSearchResultBinding.bind(view)
-        binding.ownerIconView.load(item.ownerIconUrl);
-        binding.nameView.text = item.name;
-        binding.languageView.text = item.language;
-        binding.starsView.text = "${item.stargazersCount} stars";
-        binding.watchersView.text = "${item.watchersCount} watchers";
-        binding.forksView.text = "${item.forksCount} forks";
-        binding.openIssuesView.text = "${item.openIssuesCount} open issues";
+        binding.ownerIconView.load(item.ownerIconUrl)
+        binding.nameView.text = item.name
+        binding.languageView.text = item.language
+        binding.starsView.text = "${item.stargazersCount} stars"
+        binding.watchersView.text = "${item.watchersCount} watchers"
+        binding.forksView.text = "${item.forksCount} forks"
+        binding.openIssuesView.text = "${item.openIssuesCount} open issues"
     }
 }
