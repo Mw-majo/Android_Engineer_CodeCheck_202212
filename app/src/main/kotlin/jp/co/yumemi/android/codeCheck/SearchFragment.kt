0cc6a71768704fa12c8fe4cad6e7268edb455347
Mw-majo <ritz1229@icloud.com>
2022-12-25 17:22:37 +0900

    add: CustomAdapter.kt

--- app/src/main/kotlin/jp/co/yumemi/android/codeCheck/SearchFragment.kt
+++ app/src/main/kotlin/jp/co/yumemi/android/codeCheck/SearchFragment.kt
@@ -57,39 +57,3 @@ class SearchFragment : Fragment(R.layout.fragment_search) {
         findNavController().navigate(directions)
     }
 }
-
-val diff_util = object : DiffUtil.ItemCallback<Item>() {
-    override fun areItemsTheSame(oldItem: Item, newItem: Item): Boolean {
-        return oldItem.name == newItem.name
-    }
-
-    override fun areContentsTheSame(oldItem: Item, newItem: Item): Boolean {
-        return oldItem == newItem
-    }
-}
-
-class CustomAdapter(
-    private val itemClickListener: OnItemClickListener,
-) : ListAdapter<Item, CustomAdapter.ViewHolder>(diff_util) {
-
-    class ViewHolder(view: View) : RecyclerView.ViewHolder(view)
-
-    interface OnItemClickListener {
-        fun itemClick(item: Item)
-    }
-
-    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
-        val view = LayoutInflater
-            .from(parent.context)
-            .inflate(R.layout.layout_item, parent, false)
-        return ViewHolder(view)
-    }
-
-    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
-        val item = getItem(position)
-        holder.itemView.findViewById<TextView>(R.id.repository_name_view).text = item.name
-        holder.itemView.setOnClickListener {
-            itemClickListener.itemClick(item)
-        }
-    }
-}
