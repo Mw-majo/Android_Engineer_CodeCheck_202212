e67d61b7e91ed9690578450cf6f63bb5d96acbac
Mw-majo <ritz1229@icloud.com>
2022-12-26 05:08:42 +0900

    feature: ダークモードへの対応

--- app/src/main/kotlin/jp/co/yumemi/android/codeCheck/SearchActivity.kt
+++ app/src/main/kotlin/jp/co/yumemi/android/codeCheck/SearchActivity.kt
@@ -3,9 +3,29 @@
  */
 package jp.co.yumemi.android.codeCheck
 
+import android.os.Build
+import android.os.Bundle
+import android.os.PersistableBundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.app.AppCompatDelegate
 
 /**
  * githubのレポジトリ検索用のアクティビティ
  */
-class SearchActivity : AppCompatActivity(R.layout.activity_search)
+class SearchActivity : AppCompatActivity(R.layout.activity_search) {
+
+
+    override fun onCreate(savedInstanceState: Bundle?, persistentState: PersistableBundle?) {
+        super.onCreate(savedInstanceState, persistentState)
+        setupNightMode()
+    }
+
+    private fun setupNightMode() {
+        val nightMode = if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q) {
+            AppCompatDelegate.MODE_NIGHT_FOLLOW_SYSTEM
+        } else {
+            AppCompatDelegate.MODE_NIGHT_AUTO_BATTERY
+        }
+        AppCompatDelegate.setDefaultNightMode(nightMode)
+    }
+}
