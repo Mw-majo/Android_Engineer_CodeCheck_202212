7e0e08e2204e425ebfb902de7aa26264f5dce457
Mw-majo <85604966+Mw-majo@users.noreply.github.com>
2022-12-26 00:25:58 +0900

    バグの修正：入力なし検索時にクラッシュする (#29)
    * fix: 入力がないときに検索するとクラッシュするバグ修正
    
    入力がないときに検索リクエストを送らない
    
    * ref: 使用していないライブラリの削除

--- app/src/main/kotlin/jp/co/yumemi/android/codeCheck/SearchFragment.kt
+++ app/src/main/kotlin/jp/co/yumemi/android/codeCheck/SearchFragment.kt
@@ -4,14 +4,12 @@
 package jp.co.yumemi.android.codeCheck
 
 import android.os.Bundle
-import android.view.LayoutInflater
 import android.view.View
-import android.view.ViewGroup
 import android.view.inputmethod.EditorInfo
-import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.findNavController
-import androidx.recyclerview.widget.*
+import androidx.recyclerview.widget.DividerItemDecoration
+import androidx.recyclerview.widget.LinearLayoutManager
 import jp.co.yumemi.android.codeCheck.databinding.FragmentSearchBinding
 
 class SearchFragment : Fragment(R.layout.fragment_search) {
@@ -32,15 +30,18 @@ class SearchFragment : Fragment(R.layout.fragment_search) {
             }
         )
 
+        // 入力を監視するリスナーを定義
         fragmentSearchBinding.queryEditField.setOnEditorActionListener { editText, action, _ ->
-            if (action != EditorInfo.IME_ACTION_SEARCH) {
+
+            val keyword = editText.text.toString()
+
+            // 入力がないか、ボタンが押されていなければ検索を行わない
+            if (action != EditorInfo.IME_ACTION_SEARCH || keyword == "") {
                 return@setOnEditorActionListener false
             }
 
-            editText.text.toString().let {
-                searchViewModel.getSearchResults(it).apply {
-                    customAdapter.submitList(this)
-                }
+            searchViewModel.getSearchResults(keyword).apply {
+                customAdapter.submitList(this)
             }
             return@setOnEditorActionListener true
         }
